e01fe1de8d030897f8ea9f8598ff44fb4ec1e85f
Tomas Trnecka <trnt00@vse.cz>
2023-04-12 04:17:14 +0200

    Added index.html + navbar.html

--- src/main/java/com/trnt/Main.java
+++ src/main/java/com/trnt/Main.java
@@ -2,19 +2,12 @@ package com.trnt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    @GetMapping("/status")
-    public String init(){
-        return "OK";
-    }
-
 }
